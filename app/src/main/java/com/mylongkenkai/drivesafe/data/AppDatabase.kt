5eae6a877e5ea4fb75ddbef2a6a50b8f2938e23c
reignnz <64293095+reignnz@users.noreply.github.com>
2021-10-09 18:23:41 +0800

    Include Record

--- app/src/main/java/com/mylongkenkai/drivesafe/data/AppDatabase.kt
+++ app/src/main/java/com/mylongkenkai/drivesafe/data/AppDatabase.kt
@@ -5,9 +5,10 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Exclusion::class], version = 1)
+@Database(entities = [Exclusion::class, Record::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun exclusionDao(): ExclusionDao
+    abstract fun recordDao(): RecordDao
 
     companion object {
         /**
